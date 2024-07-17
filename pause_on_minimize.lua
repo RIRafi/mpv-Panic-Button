6801d1916bb51a76637b9859fa08b3496c3f2b91
RIRafi <83538321+RIRafi@users.noreply.github.com>
2024-07-17 11:39:37 +0600

    Code refactored

--- pause_on_minimize.lua
+++ pause_on_minimize.lua
@@ -1,15 +1,16 @@
--- Define the function to toggle fullscreen
+-- Bind the function to a custom key (e.g., "f" key)
+
+key_bind = "]" --Edit this
+
+-- Define the function to toggle minimization
 function toggle_minimize()
     local fs = mp.get_property_native("window-minimized")
     mp.set_property_native("window-minimized", not fs)
 end
 
-key_bind = "]"
-
--- Bind the function to a custom key (e.g., "f" key)
 mp.add_key_binding(key_bind, "toggle_minimize", toggle_minimize)
 
-
+-- Define the function to pause when minimaztion is occurred
 function on_pause_change(name, value)
     if value == true then
         mp.set_property("pause", "yes")
